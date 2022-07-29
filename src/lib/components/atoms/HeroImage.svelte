1850e818deee9c8d679c4521a2452717124f69a2
Ushie <ushiekane@gmail.com>
2022-07-30 00:21:04 +0300

    chore: update manager image

--- src/lib/components/atoms/HeroImage.svelte
+++ src/lib/components/atoms/HeroImage.svelte
@@ -1,18 +1,16 @@
 <script>
- 
 </script>
 
-<img class="hero-img" src="/manager.png" alt="Screenshot of ReVanced Manager"/>
+<img class="hero-img" src="/manager.png" alt="Screenshot of ReVanced Manager" />
 
 <style>
 	.hero-img {
-        height: 40rem;
+		height: 40rem;
 		z-index: -1;
-        width:auto;
-		float:right;
+		width: auto;
+		float: right;
 		border-radius: 20px;
 		transform: rotate(3.7deg) translateY(2%) translateX(-15%);
 		user-select: none;
 	}
 </style>
-
