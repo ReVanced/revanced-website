508269e52d2c9a01afed3d8225ff785d97d474c9
afn <47723802+afnzmn@users.noreply.github.com>
2022-10-09 20:24:18 -0400

    BREAKING CHANGE: migrate to new routing system

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import NavHost from "$lib/components/molecules/NavHost.svelte";
+	import Wave from '$lib/components/atoms/Wave.svelte';
+
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="og:title" content="ReVanced"/>
+	<meta name="og:image" itemprop="image" content="/embed.png">
+	<meta property="og:description" content="An extensible framework for building application mods.">
+	<meta name="twitter:image" itemprop="image" content="/embed.png">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="theme-color" content="#0f111a">
+	<title>ReVanced</title>
+</svelte:head>
+
+<NavHost/>
+<slot />
+<Wave />
