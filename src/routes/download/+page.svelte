d66c063789e4e10c11c1580624e898198eaf357c
afn <47723802+afnzmn@users.noreply.github.com>
2022-10-12 23:39:20 -0400

    fix: remove reference to download selector

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-    import DownloadSelectorHost from '$lib/components/molecules/DownloadSelectorHost.svelte';
+    // import Button from "$lib/components/atoms/Button.svelte";
 </script>
 
 <div class="wrapper">
-    <DownloadSelectorHost></DownloadSelectorHost>
+    <!-- <Button icon="download" kind="primary">Download Manager v0.0.32</Button>
+    <Button>Are you a nerd?</Button> -->
 </div>
