49cc968d7f3dd83c07b97440ec7915337ba4d8f9
afn <hey@afn.lol>
2022-12-15 15:38:14 -0500

    feat: svg component

---
+++ src/lib/components/atoms/Svg.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let viewBoxHeight: number;
+	export let viewBoxWidth = viewBoxHeight;
+	export let svgHeight: number;
+	export let svgWidth = svgHeight;
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	preserveAspectRatio="none"
+	viewBox="0 0 {viewBoxHeight} {viewBoxWidth}"
+	style:height={svgHeight+'px'}
+	style:width={svgWidth+'px'}
+>
+	<slot />
+</svg>
