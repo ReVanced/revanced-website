24af6f54e44691278fb453828f7d55bff05093ed
Ax333l <main@axelen.xyz>
2023-01-06 17:13:27 +0100

    afn will explod

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-import { wrap } from './src/_vercel-moment.js';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -12,9 +10,9 @@ const config = {
 	kit: {
 		// adapter-static has vercel detection, but that does not let you set a custom 404 page easily.
 		// Instead, we have to use a wrapper that generates a vercel config if on vercel...
-		adapter: wrap(adapter, {
-			pages: "public",
-			fallback: "404.html"
+		adapter: adapter({
+			pages: 'public',
+			fallback: '404.html'
 		}),
 	}
 };
