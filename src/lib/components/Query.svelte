807843d7c84429b60c1738e972002f83c9feedb8
Ax333l <main@axelen.xyz>
2023-02-11 21:17:49 +0100

    shh

--- src/lib/components/Query.svelte
+++ src/lib/components/Query.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { CreateQueryResult } from '@tanstack/svelte-query';
 	import { isRestoring } from '../../routes/+layout.svelte';
-	// I will try to get this merged into tanstack query.
+	// I might try to get this merged into tanstack query.
 
 	// So basically, this is how you do generics here.
 	//https://github.com/sveltejs/language-tools/issues/273#issuecomment-1003496094
