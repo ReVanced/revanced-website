27eff6aa174e3c03f0843bc2f3e054ac3fde7107
Ax333l <main@axelen.xyz>
2023-02-23 19:56:36 +0100

    fix the rest of the errors

--- src/util/dev.ts
+++ src/util/dev.ts
@@ -1,7 +1,7 @@
 import { dev } from '$app/environment';
 
 // console.log, but only if in dev environment.
-export function dev_log(part: string, ...args) {
+export function dev_log(part: string, ...args: any[]) {
 	if (dev) {
 		console.log(`[${part}]:`, ...args);
 	}
