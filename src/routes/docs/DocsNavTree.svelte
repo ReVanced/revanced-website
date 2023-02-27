afc3f97fa0ca898b7fb634fa60420641664d6847
Ax333l <main@axelen.xyz>
2023-02-27 20:30:27 +0100

    style(docs tree): move info node assertion to shared file

--- src/routes/docs/DocsNavTree.svelte
+++ src/routes/docs/DocsNavTree.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
-	import { is_tree, type DocsTreeNode } from './documentation.shared';
-	import type { DocsTree, DocumentInfo } from './documentation.shared';
+	import { is_tree, assert_is_info_node } from './documentation.shared';
+	import type { DocsTree } from './documentation.shared';
 
 	import DocsNavNode from './DocsNavNode.svelte';
 
 	export let tree: DocsTree;
 	// How deeply nested this is.
 	export let nested = 0;
-
-  function assert_info_node(v: DocsTreeNode) {
-    if (!is_tree(v)) {
-      throw new Error("not tree aaa");
-    }
-    return v as DocumentInfo;
-  }
 </script>
 
 {#if nested}
@@ -32,7 +25,7 @@
 			<!-- Recursion here is fine. We are not dealing with a tree the size of a linux root file system. -->
 			<svelte:self tree={node} nested={nested + 1} />
 		{:else}
-			<DocsNavNode info={assert_info_node(node)} />
+			<DocsNavNode info={assert_is_info_node(node)} />
 		{/if}
 	{/each}
 </ul>
--- src/routes/docs/documentation.shared.ts
+++ src/routes/docs/documentation.shared.ts
@@ -1,22 +1,22 @@
 /// Types
 
 export interface Document {
-  title: string;
-  // HTML
-  content: string;
+	title: string;
+	// HTML
+	content: string;
 }
 
 export interface DocumentInfo {
-  title: string;
-  slug: string;
+	title: string;
+	slug: string;
 }
 
 // A tree representing the `docs` folder.
 export interface DocsTree {
-  // index.whatever
-  index: DocumentInfo;
-  // Everything except index.whatever
-  nodes: DocsTreeNode[];
+	// index.whatever
+	index: DocumentInfo;
+	// Everything except index.whatever
+	nodes: DocsTreeNode[];
 }
 
 export type DocsTreeNode = DocsTree | DocumentInfo;
@@ -24,5 +24,12 @@ export type DocsTreeNode = DocsTree | DocumentInfo;
 /// Functions
 
 export function is_tree(node: DocsTreeNode) {
-  return Object.prototype.hasOwnProperty.call(node, 'nodes');
+	return Object.prototype.hasOwnProperty.call(node, 'nodes');
+}
+
+export function assert_is_info_node(v: DocsTreeNode) {
+	if (!is_tree(v)) {
+		throw new Error('Value is not a tree node.');
+	}
+	return v as DocumentInfo;
 }
