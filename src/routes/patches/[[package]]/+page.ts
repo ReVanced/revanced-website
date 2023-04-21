f3665d6bda4a15688ce8b3b622f2972183ffaee5
afn <hey@afn.lol>
2023-04-20 22:41:54 -0400

    fix: do not prerender patches page

--- src/routes/patches/[[package]]/+page.ts
+++ src/routes/patches/[[package]]/+page.ts
@@ -1,5 +1,7 @@
 import type { PageLoad } from './$types';
 
+export const prerender = false;
+
 export const load: PageLoad = async ({ params }) => {
 	const selectedPkg = params.package || undefined;
 	return { selectedPkg };
