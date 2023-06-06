4f5648196ec9f1cd6c4c3f48002d47d428490690
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-06-06 00:52:38 -0400

    feat: move social icons to nav header

---
+++ src/layout/Navbar/SocialButton.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let src = 'github';
+	export let href = '#';
+</script>
+
+<a {href} rel="noreferrer" target="_blank">
+	<svg
+		height="20"
+		width="20"
+		xmlns="http://www.w3.org/2000/svg"
+		preserveAspectRatio="none"
+		viewBox="0 0 407 407"
+	>
+		<use xlink:href="socials/{src}.svg#main" />
+	</svg>
+</a>
+
+<style>
+	a:hover {
+		color: var(--accent-color-two);
+	}
+
+	a {
+		background-color: transparent;
+		border: none;
+		cursor: pointer;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: var(--grey-five);
+	}
+</style>
