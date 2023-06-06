58e6c7f4db56d14b2c06fbb20c6e272bf9eb0933
Ushie <ushiekane@gmail.com>
2023-06-06 10:54:43 +0300

    Revert telemetary for test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,6 @@
 	import { derived } from 'svelte/store';
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
 
 	import { QueryClient } from '@tanstack/query-core';
 	import { persistQueryClient } from '@tanstack/query-persist-client-core';
@@ -55,38 +54,6 @@
 		false
 	);
 </script>
-
-<!-- telemetry good -->
-<svelte:head>
-	<script>
-		(function (w, d, s, l, i) {
-			w[l] = w[l] || [];
-			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-			var f = d.getElementsByTagName(s)[0],
-				j = d.createElement(s),
-				dl = l != 'dataLayer' ? '&l=' + l : '';
-			j.async = true;
-			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-			f.parentNode.insertBefore(j, f);
-		})(window, document, 'script', 'dataLayer', 'GTM-MQ6K849');
-	</script>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-PLH0N9VQL5"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-		gtag('config', 'G-PLH0N9VQL5');
-	</script>
-	<script type="text/javascript">
-	    (function(c,l,a,r,i,t,y){
-		c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-		t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-		y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-	    })(window, document, "clarity", "script", "hfh8dhfgus");
-	</script>
-</svelte:head>
 
 <QueryClientProvider client={queryClient}>
 	<NavHost />
