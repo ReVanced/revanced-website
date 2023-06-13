af9d84453e1c14d088a6c0c1a4a0560f629d4bc3
Radu Bratan <hyperbeast.mlgboss@gmail.com>
2023-06-13 12:46:15 +0300

    fix: made modal scrollbar be hidden on all browsers

--- src/lib/components/Dialogue.svelte
+++ src/lib/components/Dialogue.svelte
@@ -134,6 +134,8 @@
 		padding: 32px;
 		box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12),
 			0px 2px 4px -1px rgba(0, 0, 0, 0.2);
+		scrollbar-width: none;
+		-ms-overflow-style: none;
 	}
 
 	button {
