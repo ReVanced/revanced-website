46d89d64b73ab82b60c73fcc010ea97e577e9adb
oSumAtrIX <johan.melkonyan1@web.de>
2023-06-25 04:23:16 +0200

    fix: remove hardcoded friendly names

--- src/util/friendlyName.ts
+++ src/util/friendlyName.ts
@@ -1,15 +1,9 @@
 export function friendlyName(text: string): string {
 	return text
 		.replace(/-/g, ' ')
-		.replace(/revanced\/revanced/g, '')
+		.replace(/revanced\/revanced/g, 'ReVanced')
 		.replace(/revanced/g, 'ReVanced')
 		.replace(/\bcli\b/g, 'CLI')
 		.replace(/api/g, 'API')
-		.replace(/microg/g, 'MicroG')
-		.replace(/hdr/g, 'HDR')
-		.replace(/sponsorblock/g, 'SponsorBlock')
-		.replace(/tiktok/g, 'TikTok')
-		.replace(/vr/g, 'VR')
-		.replace(/url/g, 'URL')
 		.replace(/(?:^|\s)\S/g, (x: string) => x.toUpperCase());
 }
