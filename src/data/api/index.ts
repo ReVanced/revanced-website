60531015568cf64785639b6ab1f874a7009139d1
Ushie <ushiekane@gmail.com>
2023-08-01 00:59:07 +0300

    feat(download): migrate to /releases endpoint

--- src/data/api/index.ts
+++ src/data/api/index.ts
@@ -1,11 +1,11 @@
 import * as settings from './settings';
 
 // API Endpoints
-import type { Patch, Repository, Tool } from '$lib/types';
+import type { Patch, Repository, Metadata, Asset } from '$lib/types';
 
 export type ReposData = Repository[];
 export type PatchesData = { patches: Patch[]; packages: string[] };
-export type ToolsData = { [repo: string]: Tool };
+export type ReleaseData = { metadata: Metadata; assets: Asset[] };
 
 async function get_json(endpoint: string) {
 	const url = `${settings.api_base_url()}/${endpoint}`;
@@ -16,39 +16,11 @@ async function repositories(): Promise<ReposData> {
 	return await get_json('contributors').then((json) => json.repositories);
 }
 
-async function tools(): Promise<ToolsData> {
-	const json = await get_json('tools');
-	// Make the data easier to work with.
-	const map: Map<string, Tool> = new Map();
-	for (const tool of json['tools']) {
-		const repo: string = tool.repository;
-
-		if (!map.has(repo)) {
-			map.set(repo, {
-				version: tool.version,
-				repository: repo,
-				// Just use the timestamp of the first one we find.
-				timestamp: tool.timestamp,
-				assets: []
-			});
-		}
-
-		const value = map.get(repo)!!;
-		value.assets.push({
-			name: tool.name,
-			size: tool.size,
-			url: tool.browser_download_url,
-			content_type: tool.content_type
-		});
-
-		map.set(repo, value);
-	}
-
-	return Object.fromEntries(map);
-}
-
-async function manager(): Promise<Tool> {
-	return await tools().then((data) => data['revanced/revanced-manager']);
+async function manager(): Promise<ReleaseData> {
+	const json = await get_json('v2/revanced-manager/releases/latest');
+	// console.log(json.release.metadata.tag_name);
+	console.log(json.release.assets[0].browser_download_url);
+	return { metadata: json.release.metadata, assets: json.release.assets };
 }
 
 async function patches(): Promise<PatchesData> {
