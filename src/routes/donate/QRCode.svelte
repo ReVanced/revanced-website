ce1a45cbfff7c910893d8d5f8612af617d64070e
Ushie <ushiekane@gmail.com>
2023-08-06 17:26:58 +0300

    feat: use better QR lib

--- src/routes/donate/QRCode.svelte
+++ src/routes/donate/QRCode.svelte
@@ -1,37 +1,17 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
+	import QRious from 'qrious/dist/qrious';
 
-	export let codeValue;
-	export let squareSize;
-
-	let qrcode;
+	export let codeValue: string;
+	export let squareSize: number = 200;
 
 	onMount(() => {
-		let script = document.createElement('script');
-		script.src = 'https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js';
-		document.head.append(script);
-
-		script.onload = function () {
-			qrcode = new QRCode('qrcode', {
-				text: codeValue,
-				width: squareSize,
-				height: squareSize,
-				colorDark: '#000000',
-				colorLight: '#ffffff',
-				correctLevel: QRCode.CorrectLevel.H
-			});
-		};
+		new QRious({
+			element: document.getElementById('qrcode'),
+			value: codeValue,
+			size: squareSize
+		});
 	});
 </script>
 
-<div id="qrcode" />
-
-<style>
-	#qrcode {
-		background-color: white;
-		padding: 10px;
-		width: 220px;
-		height: 220px;
-		border-radius: 0.5rem;
-	}
-</style>
+<canvas id="qrcode" style="border-radius: 0.5rem;" />
