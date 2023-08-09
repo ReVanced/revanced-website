f08626dfb93d537444a2805c843b6cf384bf1a4f
afn <hey@afn.lol>
2023-08-09 11:25:53 -0400

    add card image fallback

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -65,7 +65,7 @@
 					<a class="donate-card" target="_blank" rel="noreferrer" href={platform.url}>
 						<!-- not using <img/> because we want the image height to always be 200px -->
 						<div
-							style="background-image: url('/donate/card-images/{platform.name}.png');"
+							style="background-image: url('/donate/card-images/{platform.name}.png'), url('/donate/card-images/fallback.png');"
 							role="img"
 							aria-label="{platform.name} preview image"
 						/>
@@ -76,7 +76,7 @@
 			{#if data.wallets}
 				<button class="donate-card" on:click={() => (cryptoDialogue = !cryptoDialogue)}>
 					<div
-						style="background-image: url('/donate/card-images/Cryptocurrencies.png');"
+						style="background-image: url('/donate/card-images/Cryptocurrencies.png'), url('/donate/card-images/fallback.png');"
 						role="img"
 						aria-label="Cryptocurrencies preview image"
 					/>
@@ -121,7 +121,7 @@
 						<div class="wallet-name">
 							<img
 								src="/donate/crypto/{wallet.currency_code}.svg"
-								onerror="this.onerror=null; this.src='/donate/generic.svg'"
+								onerror="this.onerror=null; this.src='/donate/fallback.svg'"
 								alt={`${wallet.network} icon.'`}
 							/>
 							{`${wallet.network} (${wallet.currency_code})`}
