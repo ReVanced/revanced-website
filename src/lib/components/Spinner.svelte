d808b318bc4404d69349a906568591c9825c8275
Niyas Hameed <niyasrad@gmail.com>
2023-08-10 00:10:31 +0530

    fix(Spinner): properly center align (#81)

--- src/lib/components/Spinner.svelte
+++ src/lib/components/Spinner.svelte
@@ -11,15 +11,22 @@
 		}
 	}
 
-	.spinner:before {
-		content: '';
-		box-sizing: border-box;
-		width: 50px;
-		height: 50px;
-		border-radius: 50%;
+	.spinner {
 		position: fixed;
 		top: 50%;
 		left: 50%;
+		width: 50px;
+		height: 50px;
+		transform: translate(-50%, -50%);
+	}
+
+	.spinner:before {
+		content: '';
+		box-sizing: border-box;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
 		border: 4.5px solid transparent;
 		border-top-color: var(--accent-color);
 		animation: spinner 0.6s linear infinite;
