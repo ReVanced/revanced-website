dfb05c20d682da15aadcb5457d134b9b43ced9ea
madkarmaa <madkarma99@gmail.com>
2024-08-18 15:01:22 +0200

    feat: show latest announcement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,6 +43,9 @@
 		if (allow) location.reload();
 	}
 
+	const fetchLatestAnnouncement = async () =>
+		await (await fetch('https://api.revanced.app/v2/announcements/latest')).json();
+
 	onMount(() => {
 		new DateTriggerEventHandler(themeEvents);
 
@@ -94,6 +97,19 @@
 		}
 	</script>
 </svelte:head>
+
+{#await fetchLatestAnnouncement() then data}
+	<!-- ====================================================================== idk what else to put here to prevent an exception from trying to parse null -->
+	{#if data['id'] !== parseInt(localStorage.getItem('last_shown_announcement_id') || '69696969')}
+		<Banner
+			on:dismissed={() => localStorage.setItem('last_shown_announcement_id', String(data['id']))}
+		>
+			{data['title']}
+			<!-- TODO: change href to announcement page -->
+			<a href="." target="_blank" rel="noopener noreferrer">Read more</a>.
+		</Banner>
+	{/if}
+{/await}
 
 {#await fetch('https://api.revanced.app/v3/ping', { method: 'HEAD' }) then res}
 	{#if !res.ok}
