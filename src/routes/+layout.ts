ba8781c3996964d5c0a3d1f5abe6d0b225c4c3a3
madkarmaa <madkarma99@gmail.com>
2024-08-19 15:53:26 +0200

    fix: remove unspecified fetch warning

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,8 @@
 export const prerender = true;
 
 // https://kit.svelte.dev/docs/load#layout-data
-export async function load() {
+// https://kit.svelte.dev/docs/load#making-fetch-requests
+export async function load({ fetch }) {
 	return {
 		ping: await fetch('https://api.revanced.app/v3/ping', { method: 'HEAD' }),
 		online_announcement: await (
