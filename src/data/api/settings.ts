05c65137ad72dcc07ac9817d8d13e7de2972f95f
madkarmaa <madkarma99@gmail.com>
2024-09-20 19:55:00 +0200

    refactor: remove parentheses and newlines

--- src/data/api/settings.ts
+++ src/data/api/settings.ts
@@ -7,36 +7,24 @@ export const default_base_url = RV_API_URL;
 
 // Get base URL
 export function api_base_url(): string {
-	if (browser) {
-		return localStorage.getItem(URL_KEY) || default_base_url;
-	}
-
+	if (browser) return localStorage.getItem(URL_KEY) || default_base_url;
 	return default_base_url;
 }
 
 // (Re)set base URL.
 export function set_api_base_url(url?: string) {
-	if (!url) {
-		localStorage.removeItem(URL_KEY);
-	} else {
-		localStorage.setItem(URL_KEY, url);
-	}
+	if (!url) localStorage.removeItem(URL_KEY);
+	else localStorage.setItem(URL_KEY, url);
 }
 
 // Get access token.
 export function get_access_token(): string | null {
-	if (browser) {
-		return localStorage.getItem('revanced_api_access_token');
-	}
-
+	if (browser) return localStorage.getItem('revanced_api_access_token');
 	return null;
 }
 
-// Set access token.
+// (Re)set access token.
 export function set_access_token(token?: string) {
-	if (!token) {
-		localStorage.removeItem('revanced_api_access_token');
-	} else {
-		localStorage.setItem('revanced_api_access_token', token);
-	}
+	if (!token) localStorage.removeItem('revanced_api_access_token');
+	else localStorage.setItem('revanced_api_access_token', token);
 }
