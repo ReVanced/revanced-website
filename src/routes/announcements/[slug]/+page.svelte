24984e3e52010a0017089dbf5908c01e416ac256
madkarmaa <madkarma99@gmail.com>
2024-11-12 18:38:57 +0100

    feat: move gallery inside announcement container

--- src/routes/announcements/[slug]/+page.svelte
+++ src/routes/announcements/[slug]/+page.svelte
@@ -46,11 +46,27 @@
 			<div class="content">
 				{@html data.announcement?.content || ''}
 			</div>
-		</div>
 
-		{#if data.announcement.attachments.length > 0}
-			<Gallery images={data.announcement.attachments} />
-		{/if}
+			<svg
+				aria-hidden="true"
+				width="100%"
+				height="8"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<pattern id="a" width="91" height="8" patternUnits="userSpaceOnUse">
+					<path
+						d="M114 4c-5.067 4.667-10.133 4.667-15.2 0S88.667-.667 83.6 4 73.467 8.667 68.4 4 58.267-.667 53.2 4 43.067 8.667 38 4 27.867-.667 22.8 4 12.667 8.667 7.6 4-2.533-.667-7.6 4s-10.133 4.667-15.2 0S-32.933-.667-38 4s-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0"
+						stroke-linecap="square"
+					/>
+				</pattern>
+				<rect width="100%" height="100%" fill="url(#a)" />
+			</svg>
+
+			{#if data.announcement.attachments.length > 0}
+				<Gallery images={data.announcement.attachments} />
+			{/if}
+		</div>
 	</Query>
 </main>
 
