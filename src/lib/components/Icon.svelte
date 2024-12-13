1e813e8991aab7d2d38a49d755af9afd3c53da74
madkarmaa <madkarma99@gmail.com>
2024-12-13 20:16:54 +0100

    feat: add Icon component

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<span class="material-symbols-outlined" {...$$restProps}>{name}</span>
+
+<style>
+	.material-symbols-outlined {
+		font-variation-settings:
+			'FILL' 0,
+			'wght' 400,
+			'GRAD' 0,
+			'opsz' 24;
+	}
+</style>
