8902901acf17c1b333ac9f538cdd46cda1e7db41
madkarmaa <madkarma99@gmail.com>
2025-04-03 12:11:09 +0200

    feat: init components structure

---
+++ src/lib/components/templates/Page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import NavBar from '$components/organisms/NavBar.svelte';
+	import Footer from '$components/organisms/Footer.svelte';
+
+	type Props = { children: Snippet };
+	let { children }: Props = $props();
+</script>
+
+<main>
+	<NavBar />
+	{@render children()}
+	<Footer />
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$components: './src/lib/components',
+			$api: './src/lib/api',
+			$lib: './src/lib'
+		}
+	}
 };
 
 export default config;
