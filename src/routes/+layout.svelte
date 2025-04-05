bbb999a2093238030801aad237657fe4ee1b2704
madkarmaa <madkarma99@gmail.com>
2025-04-05 12:17:21 +0200

    fix: use `$effect` instead of `onMount`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { theme } from '$lib/stores';
-	import { onMount, type Snippet } from 'svelte';
+	import type { Snippet } from 'svelte';
 
 	type Props = { children: Snippet };
 	let { children }: Props = $props();
 
-	onMount(() => {
+	$effect(() => {
 		document.documentElement.setAttribute('data-theme', $theme);
 	});
 </script>
