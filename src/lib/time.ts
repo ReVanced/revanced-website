e36417bfb2df2072fd0fb89f7563ac27b594d87e
madkarmaa <madkarma99@gmail.com>
2025-04-05 12:42:56 +0200

    feat: add `weeks` fn

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -2,3 +2,4 @@ export const seconds = (seconds: number): number => seconds * 1000;
 export const minutes = (minutes: number): number => seconds(minutes * 60);
 export const hours = (hours: number): number => minutes(hours * 60);
 export const days = (days: number): number => hours(days * 24);
+export const weeks = (weeks: number): number => days(weeks * 7);
