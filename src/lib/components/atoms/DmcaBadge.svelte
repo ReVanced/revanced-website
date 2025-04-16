3b65d0095d03f976ec90b924098047aab3341a06
madkarmaa <madkarma99@gmail.com>
2025-04-16 11:42:02 +0200

    feat: add DMCA badge

---
+++ src/lib/components/atoms/DmcaBadge.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	const RV_DMCA_GUID = 'test'; // TODO: replace later
+
+	let refurl: string = $state('');
+	$effect(() => {
+		refurl = window.location.href;
+	});
+</script>
+
+<a
+	href="//www.dmca.com/Protection/Status.aspx?ID={RV_DMCA_GUID}&refurl={refurl}"
+	title="DMCA.com Protection Status"
+	class="dmca-badge"
+>
+	<img
+		src="https://images.dmca.com/Badges/dmca-badge-w150-5x1-08.png?ID={RV_DMCA_GUID}"
+		alt="DMCA.com Protection Status"
+	/></a
+>
--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import DmcaBadge from '$components/atoms/DmcaBadge.svelte';
 	import SquigglyDivider from '$components/atoms/SquigglyDivider.svelte';
 </script>
 
 <footer>
 	<SquigglyDivider />
+	<DmcaBadge />
 </footer>
