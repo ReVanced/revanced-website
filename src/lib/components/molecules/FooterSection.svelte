ef9bf6ba0ca2ec2f3cf786acae665dd28f0c72ec
madkarmaa <madkarma99@gmail.com>
2025-05-04 17:52:18 +0200

    feat: add mobile dropdown

--- src/lib/components/molecules/FooterSection.svelte
+++ src/lib/components/molecules/FooterSection.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
+	import { slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+
+	import ChevronDown from '~icons/mdi/chevron-down';
 
 	type Props = { title: string; children: Snippet };
 	let { title, children }: Props = $props();
@@ -12,4 +16,20 @@
 	<ul>
 		{@render children()}
 	</ul>
+</div>
+
+<div class="mobile-only">
+	<button class="title" onclick={() => (expanded = !expanded)}>
+		<span>
+			{title}
+		</span>
+		<div class="arrow" style:transform={expanded ? 'rotate(180deg)' : 'rotate(0deg)'}>
+			<ChevronDown color="var(--surface-six)" />
+		</div>
+	</button>
+	{#if expanded}
+		<ul transition:slide={{ easing: quintOut, duration: 500 }}>
+			{@render children()}
+		</ul>
+	{/if}
 </div>
