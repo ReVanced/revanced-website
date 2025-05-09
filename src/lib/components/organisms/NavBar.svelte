485ac26c30bbb85c4d61f06ab51a420fcf5daff4
madkarmaa <madkarma99@gmail.com>
2025-05-09 11:40:34 +0200

    refactor: better banner logic

--- src/lib/components/organisms/NavBar.svelte
+++ src/lib/components/organisms/NavBar.svelte
@@ -4,13 +4,23 @@
 	import api from '$api';
 </script>
 
-{#await api.general.ping() then apiUp}
-	{#if !apiUp}
-		<Banner level="caution" permanent>
-			The API is currently unresponsive and some services may not work correctly.
-			{#await api.general.getAbout() then data}
-				Check the <Button type="text" href={data.status}>status page</Button> for updates.
-			{/await}
-		</Banner>
-	{/if}
-{/await}
+<!-- make the banner a snippet to remove duplicate code -->
+{#snippet apiDownBanner()}
+	<Banner level="caution" permanent>
+		The API is currently unresponsive and some services may not work correctly.
+		{#await api.general.getAbout() then data}
+			Check the <Button type="text" href={data.status}>status page</Button> for updates.
+		{/await}
+	</Banner>
+{/snippet}
+
+<div class="nav-container">
+	<!-- whether the api doesn't return 204 or the request itself fails, show banner -->
+	{#await api.general.ping() then apiUp}
+		{#if !apiUp}
+			{@render apiDownBanner()}
+		{/if}
+	{:catch _}
+		{@render apiDownBanner()}
+	{/await}
+</div>
