ff58050f130f2219cb2f88619f542cc9c5dd7853
madkarmaa <madkarma99@gmail.com>
2025-05-09 11:33:32 +0200

    refactor: fetch status page

--- src/lib/components/organisms/NavBar.svelte
+++ src/lib/components/organisms/NavBar.svelte
@@ -2,17 +2,15 @@
 	import Banner from '$components/organisms/Banner.svelte';
 	import Button from '$components/atoms/Button.svelte';
 	import api from '$api';
-
-	const statusUrl = 'https://status.revanced.app/'; // TODO: replace with env variable
 </script>
 
 {#await api.general.ping() then apiUp}
 	{#if !apiUp}
 		<Banner level="caution" permanent>
 			The API is currently unresponsive and some services may not work correctly.
-			{#if statusUrl}
-				Check the <Button type="text" href={statusUrl}>status page</Button> for updates.
-			{/if}
+			{#await api.general.getAbout() then data}
+				Check the <Button type="text" href={data.status}>status page</Button> for updates.
+			{/await}
 		</Banner>
 	{/if}
 {/await}
