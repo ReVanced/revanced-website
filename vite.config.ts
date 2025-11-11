bd50badb71220be207ceaa501403d4972857050a
madkarmaa <madkarma99@gmail.com>
2025-11-11 13:59:34 +0100

    feat: add unplugin icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), Icons({ compiler: 'svelte' })]
 });
