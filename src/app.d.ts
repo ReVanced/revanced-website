5626ac6bf34912e20556e01b7f2641269e0c114e
madkarmaa <madkarma99@gmail.com>
2025-11-11 14:22:54 +0100

    fix: add unplugin icons types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
+import 'unplugin-icons/types/svelte';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
 declare global {
 	namespace App {
 		// interface Error {}
