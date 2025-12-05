39c88f985b7bb05d9172a2187176bc029a483b48
mostafaNazari702 <nazarimostafa2006@gmail.com>
2025-12-05 19:54:44 +0100

    A small code so i could establish connection with the API without CORS issues in dev. environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
 	plugins: [sveltekit(), Icons({ compiler: 'svelte' })]
+	,
+	server: {
+		proxy: {
+			// to be removed in production
+			// Proxy API requests to bypass CORS in development
+			'/api/revanced': {
+				target: 'https://api.revanced.app',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/revanced/, ''),
+				secure: true
+			}
+		}
+	}
 });
