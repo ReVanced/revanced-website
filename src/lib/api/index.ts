7b95276451c58cb58038045d7754336a0399f1d7
mostafaNazari702 <nazarimostafa2006@gmail.com>
2025-12-05 19:59:59 +0100

    API-related stuff, such as obtaining data

---
+++ src/lib/api/index.ts
@@ -0,0 +1,11 @@
+export * from './types';
+export * from './client';
+export { 
+	getApiBaseUrl, 
+	setApiBaseUrl, 
+	buildUrl,
+	getStatusUrl,
+	getContactEmail,
+	DEFAULT_STATUS_URL,
+	DEFAULT_EMAIL
+} from './settings';
